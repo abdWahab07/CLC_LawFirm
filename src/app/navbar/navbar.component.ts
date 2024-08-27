013e022cbaa193f7333feb5c379dd500e037ad6a
abdwahab07 <abdwahab0472@gmail.com>
2024-08-26 22:52:47 -0700

    footer and navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-
+  logo = 'assets/logo-transparent.png';
 }
