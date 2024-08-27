482d3790629a0537e51a6c13bd1916f37d062508
abdwahab07 <abdwahab0472@gmail.com>
2024-08-26 23:38:18 -0700

    change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,28 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
-import { HeaderComponent } from './header/header.component';
-import { HomeCardsComponent } from './home-cards/home-cards.component';
-import { TestimonyComponent } from './testimony/testimony.component';
 import { NewsComponent } from './news/news.component';
-import { FirmComponent } from './firm/firm.component';
-import { LawyersComponent } from './lawyers/lawyers.component';
-import { PracticeAreasComponent } from './practice-areas/practice-areas.component';
-import { ContactUsComponent } from './contact-us/contact-us.component';
-
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     FooterComponent,
-    HeaderComponent,
-    HomeCardsComponent,
-    TestimonyComponent,
-    NewsComponent,
-    FirmComponent,
-    LawyersComponent,
-    PracticeAreasComponent,
-    ContactUsComponent
+    NewsComponent
   ],
   imports: [
     BrowserModule,
