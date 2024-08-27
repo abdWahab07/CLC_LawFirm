98724b528fe0a1057e6cd863ecd1dc4d131b1765
abdwahab07 <abdwahab0472@gmail.com>
2024-08-27 00:19:55 -0700

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
+import { FirmComponent } from './firm/firm.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeCardsComponent } from './home-cards/home-cards.component';
+import { LawyersComponent } from './lawyers/lawyers.component';
+import { PracticeAreasComponent } from './practice-areas/practice-areas.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeCardsComponent },
+  { path: 'lawyers', component: LawyersComponent },
+  { path: 'the-firm', component: FirmComponent },
+  { path: 'practice-areas', component: PracticeAreasComponent },
+  { path: 'contact-us', component: ContactUsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
